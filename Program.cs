fc0bdc0084dda6c8e6ff761935c70231b9ba101e
Abraham Huerta <hueabrw@dunwoody.edu>
2020-04-08 02:04:21 -0500

    added comments

--- Program.cs
+++ Program.cs
@@ -4,24 +4,29 @@ namespace mpls_dog_boarding_compute_rate_assignment_2_1_hueabrw
 {
     class Program
     {
+        //Constant rate variables
         public const float FLAT_RATE = 75.00f;
         public const float A_RATE = 169.00f;
         public const float C_RATE = 112.00f;
+
+        //Main logic of the program
         static void Main(string[] args)
         {
             Welcome();
+
             int days = PromptDays();
-            
             float rate = PromptAddOn();
 
+            //New incoive object is declared
             Invoice invoice = new Invoice(days, rate);
             EndProgram(invoice);
         }
 
+        //compute rate method
         public static float computeRate(){
             return FLAT_RATE;
         }
-
+        //overloaded compute rate method
         public static float computeRate(char addOn){
             if(addOn == 'A'){
                 return A_RATE;
@@ -29,19 +34,23 @@ namespace mpls_dog_boarding_compute_rate_assignment_2_1_hueabrw
                 return C_RATE;
             }else{
                 Console.WriteLine("Invalid option, please select select either the A add-on service or the C add-on service");
-                return computeRate(Console.ReadKey().KeyChar);
+                return computeRate(char.ToUpper(Console.ReadKey().KeyChar));
             }
         }
 
+        //Welcome module
         public static void Welcome(){
+            Console.Clear();
             Console.WriteLine("Welcome to  MPLS Dogboarding academy!");
             Console.WriteLine("\n\npress any key to begin the program...");
             Console.ReadKey();
         }
+        //Prompt for user to enter number of days
         public static int PromptDays(){
             Console.WriteLine("\n\nHow many days is your dog staying?");
             return Convert.ToInt32(Console.ReadLine());
         }
+        //prompt to see which add-on the user would like
         public static float PromptAddOn(){
             Console.WriteLine("Would you like any add-on services?(y/n)");
 
@@ -56,6 +65,7 @@ namespace mpls_dog_boarding_compute_rate_assignment_2_1_hueabrw
                 return PromptAddOn();
             }
         }
+        //Ending module
         public static void EndProgram(Invoice invoice){
             Console.WriteLine("\n" + invoice.ToString());
             Console.WriteLine("\n\npress any key to end program...");
@@ -63,6 +73,7 @@ namespace mpls_dog_boarding_compute_rate_assignment_2_1_hueabrw
         }
     }
 
+    //Invoice class
     class Invoice{
 
         public int days;
